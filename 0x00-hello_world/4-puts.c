7ad0d6db9eff8f8d60cbfcd204bf9fc23530dd5b
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-02-16 13:59:48 -0800

    Add documentations to task 4 solution

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
