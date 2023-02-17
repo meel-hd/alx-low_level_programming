0ec53582edf5a010d3a0441e7507a7708bde5e9a
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-02-17 15:12:51 -0800

    Add task 11 and 12 files

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int zero = 48;
+	int nine = 57;
+
+	while (zero <= nine)
+	{
+		int i;
+
+		for (i = 48; i <= nine; i++)
+		{
+			putchar(zero);
+			putchar(i);
+			if (zero != nine || i != nine)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		zero++;
+	}
+	putchar('\n');
+
+	return (0);
+}
