0589e74b3a324f113f01ebcc3a9d11c0f1d7c41d
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-02-17 13:22:47 -0800

    Fix compiler error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,12 +11,12 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_digit = n % 10;
-
+	last_digit = n % 10;
 	if (last_digit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
