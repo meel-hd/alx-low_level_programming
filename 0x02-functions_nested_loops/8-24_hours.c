236adec4b8fe950ab4af3df0150a867a50a92a85
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-02-21 13:13:19 -0800

    Task 8 half solution

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print minutes of the day
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int hours = 24;
+	int minutes = 60;
+	int h_count = 0;
+	int m_count;
+	int colon = 58;
+	int newline = 10;
+
+	while ( h_count < hours)
+	{
+		m_count = 0;
+		while (m_count < minutes)
+		{
+			if (h_count < 10)
+			{
+				_putchar(48);
+			}
+			_putchar(h_count + '0');
+			_putchar(colon);
+			if (m_count < 10)
+			{
+				_putchar(48);
+			}
+			_putchar(m_count + '0');
+			_putchar(newline);
+
+			m_count++;
+		}
+		h_count++;
+	}
+}
