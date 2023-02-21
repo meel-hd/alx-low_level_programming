bf1ff574db601fc4fb47e15615d17ada09a22b93
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-02-21 13:28:07 -0800

    Task 10 sol

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,14 @@
 /**
  * add - adds two intigers
  *
+ * @n: first number
+ * @j: second number
+ *
  * Return: the result
  */
 int add(int n, int j)
 {
 	int result = n + j;
-	
-	return(result);
+
+	return (result);
 }
