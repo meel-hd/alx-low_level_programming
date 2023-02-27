74e5d702de995a0fd28bc8bf50e4ffa38fa907c6
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-02-27 12:56:04 -0800

    Task 1 sol

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ * @a: first int
+ * @b: second int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
