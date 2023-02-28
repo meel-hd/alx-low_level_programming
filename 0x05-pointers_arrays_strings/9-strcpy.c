d6d43ef6cfb09a2e5a34eb62a69c4e1c897ff17e
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-02-27 16:34:06 -0800

    Task 9 sol

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string
+ * @dest: destination
+ * @src: source string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (1)
+	{
+		dest[i] = src[i];
+		if (src[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+
+	return (dest);
+}
