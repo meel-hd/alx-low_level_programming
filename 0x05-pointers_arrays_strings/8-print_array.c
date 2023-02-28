5dba406e84b7e71b5c7e9e7ddf1b36a184a4b679
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-02-27 16:21:03 -0800

    Task 8 sol

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints an array
+ * @a: the array
+ * @n: array length
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
