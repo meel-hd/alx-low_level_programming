c8383d17a092b17874f427ea83932ef7e79769fd
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-03-06 09:16:32 -0800

    Task 2 sol

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - find a char in a string
+ * @s: the string to search in
+ * @c: the char to search for
+ * Return: pointer to the first occurence
+ * or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+
+	return (0);
+}
