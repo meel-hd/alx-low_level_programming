3ad951f678a6dd2f80eb46a5788772dcf33116d7
Mehdi EL OUALY <mehdieloualy@gmail.com>
2023-03-08 10:20:37 -0800

    Task 6 sol

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if a number is a prime number
+ * @n: the number to check
+ * Return: 1 if the number is a prime number
+ * 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	return (n - n - 1);
+}
